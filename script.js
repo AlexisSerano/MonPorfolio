1bc2c80bdcdb2115420031e6c40c576830537ea5
Alexis Serano <142439879+AlexisSerano@users.noreply.github.com>
2024-02-17 12:30:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-$("titre").each(function(index, element){
-    var animation = TweenMax.to(this, 0.2, {
-      className: '+= superShadow',
-      marginTop: '-10px',
-      marginBottom: '10px',
-      ease: Power1.easeIn,
-      paused:true
-    });
-    element.animation = animation;
-  })
-  
-  
-  $('titre').hover(function(){
-   this.animation.play()
-  }, function(){
-   this.animation.reverse();
-  })
+
